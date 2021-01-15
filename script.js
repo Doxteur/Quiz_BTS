88a9af1b7e807d460b92350d4b29dd333261becd
Doxteur <Doxteurn@gmail.com>
2021-01-15 23:57:48 +0100

    Bug Fix For last question

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // Play Quiz script Temporary
-var questionNumber = 0;
+var questionNumber = 1;
+var numberOfGoodAnswer = 0;
 // Jimmy1
 function Quiz(reponse) {
-    console.log(questionNumber);
     fetch("quiz.json")
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        if (reponse == data[questionNumber].reponses[0].ReponsesVraie) {
-            console.log("Bravo !!  1+");
-            Quiz(reponse - 1);
+        console.log(data[questionNumber - 1].reponses[0].ReponsesVraie);
+        if (reponse == 5) {
+            console.log(questionNumber);
+            console.log("Start of the quiz");
+            document.getElementById("start").style.display = "none";
+            document.getElementById("content").style.display = "initial";
+            document.getElementById("titreQuestion").innerHTML = data[0].nom;
+            document.getElementById("Question").innerHTML = data[0].question;
+            for (var i = 0; i < 4; i++) {
+                var emplacement = i + 1;
+                document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
+            }
             return 1;
         }
-        else if (reponse != undefined || null) {
+        if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
+            console.log("Bravo !!  1+");
+            numberOfGoodAnswer++;
+        }
+        else {
             console.log("Mauvaise Réponse !");
         }
         document.getElementById("start").style.display = "none";
@@ -21,6 +34,11 @@ function Quiz(reponse) {
         for (var i = 0; i < 4; i++) {
             var emplacement = i + 1;
             document.getElementById(emplacement.toString()).innerHTML = data[questionNumber].reponses[0].Propositions[i];
+        }
+        if (questionNumber + 1 === data.length) {
+            console.log(numberOfGoodAnswer);
+            console.log("Finie !");
+            return;
         }
         questionNumber++;
     });
--- script.ts
+++ script.ts
@@ -1,21 +1,37 @@
 // Play Quiz script Temporary
-let questionNumber:number = 0;
-
+let questionNumber: number = 1;
+let numberOfGoodAnswer = 0;
 // Jimmy1
 
-function Quiz(reponse?) : any {
-
-    console.log(questionNumber);
+function Quiz(reponse?): any {
+    
+    
     fetch("quiz.json")
-        .then(response => response.json())
-        .then(data => {
+    .then(response => response.json())
+    .then(data => {
+            console.log(data[questionNumber -1].reponses[0].ReponsesVraie);
 
-
-            if (reponse == data[questionNumber].reponses[0].ReponsesVraie) {
-                console.log("Bravo !!  1+");
-                Quiz(reponse - 1);
+            if (reponse == 5) {
+                console.log(questionNumber);
+                console.log("Start of the quiz");
+                document.getElementById("start").style.display = "none";
+                document.getElementById("content").style.display = "initial";
+                document.getElementById("titreQuestion").innerHTML = data[0].nom;
+                document.getElementById("Question").innerHTML = data[0].question;
+                for (let i = 0; i < 4; i++) {
+                    let emplacement: number = i + 1;
+                    document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
+                }
                 return 1;
-            } else if (reponse != undefined || null) {
+            }
+
+
+
+
+            if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
+                console.log("Bravo !!  1+");
+                numberOfGoodAnswer++;
+            } else {
                 console.log("Mauvaise Réponse !");
             }
             document.getElementById("start").style.display = "none";
@@ -24,12 +40,18 @@ function Quiz(reponse?) : any {
             document.getElementById("Question").innerHTML = data[questionNumber].question;
 
             for (let i = 0; i < 4; i++) {
-                let emplacement :number = i + 1;
-               
+                let emplacement: number = i + 1;
+
                 document.getElementById(emplacement.toString()).innerHTML = data[questionNumber].reponses[0].Propositions[i];
             }
-
+            if (questionNumber + 1 === data.length) {
+                console.log(numberOfGoodAnswer);
+                console.log("Finie !");
+                return;
+            }
             questionNumber++;
+
+
 
         });
 
