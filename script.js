d7366c339b61f03411aa688e3a2aad21a0c2fbbd
jimmy doussain <doxteurn@gmail.com>
2021-01-17 20:55:31 +0000

    End Menu

--- script.js
+++ script.js
@@ -4,42 +4,48 @@ var numberOfGoodAnswer = 0;
 // Jimmy1
 function Quiz(reponse) {
     fetch("quiz.json")
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
-        console.log(data[questionNumber - 1].reponses[0].ReponsesVraie);
-        if (reponse == 5) {
-            console.log(questionNumber);
-            console.log("Start of the quiz");
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            if (questionNumber === data.length) {
+                console.log(numberOfGoodAnswer);
+                console.log("Finie !");
+                document.getElementById("ecranFin").style.display = "initial";
+                document.getElementById("nombreDeReponse").innerHTML += numberOfGoodAnswer;
+
+                document.getElementById("content").style.display = "none";
+
+                return;
+            }
+            if (reponse == 5) {
+                console.log(questionNumber);
+                console.log("Start of the quiz");
+                document.getElementById("start").style.display = "none";
+                document.getElementById("content").style.display = "initial";
+                document.getElementById("titreQuestion").innerHTML = data[0].nom;
+                document.getElementById("Question").innerHTML = data[0].question;
+                for (var i = 0; i < 4; i++) {
+                    var emplacement = i + 1;
+                    document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
+                }
+                return 1;
+            }
+            if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
+                console.log("Bravo !!  1+");
+                numberOfGoodAnswer++;
+            } else {
+                console.log("Mauvaise Réponse !");
+            }
             document.getElementById("start").style.display = "none";
             document.getElementById("content").style.display = "initial";
-            document.getElementById("titreQuestion").innerHTML = data[0].nom;
-            document.getElementById("Question").innerHTML = data[0].question;
+            document.getElementById("titreQuestion").innerHTML = data[questionNumber].nom;
+            document.getElementById("Question").innerHTML = data[questionNumber].question;
             for (var i = 0; i < 4; i++) {
                 var emplacement = i + 1;
-                document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
+                document.getElementById(emplacement.toString()).innerHTML = data[questionNumber].reponses[0].Propositions[i];
             }
-            return 1;
-        }
-        if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
-            console.log("Bravo !!  1+");
-            numberOfGoodAnswer++;
-        }
-        else {
-            console.log("Mauvaise Réponse !");
-        }
-        document.getElementById("start").style.display = "none";
-        document.getElementById("content").style.display = "initial";
-        document.getElementById("titreQuestion").innerHTML = data[questionNumber].nom;
-        document.getElementById("Question").innerHTML = data[questionNumber].question;
-        for (var i = 0; i < 4; i++) {
-            var emplacement = i + 1;
-            document.getElementById(emplacement.toString()).innerHTML = data[questionNumber].reponses[0].Propositions[i];
-        }
-        if (questionNumber + 1 === data.length) {
-            console.log(numberOfGoodAnswer);
-            console.log("Finie !");
-            return;
-        }
-        questionNumber++;
-    });
+            questionNumber++;
+        });
 }
