40f7275ef6f1c4d51d11742753f2e372758a05c0
jimmy doussain <doxteurn@gmail.com>
2021-01-18 14:23:40 +0000

    Transform in function

--- script.js
+++ script.js
@@ -11,33 +11,14 @@ function Quiz(reponse) {
         return response.json();
     })
         .then(function (data) {
+        //Start the Game 
         if (reponse == 5) {
-            console.log(questionNumber);
-            console.log("Start of the quiz");
-            document.getElementById("start").style.display = "none";
-            document.getElementById("content").style.display = "initial";
-            document.getElementById("titreQuestion").innerHTML = data[0].nom;
-            document.getElementById("Question").innerHTML = data[0].question;
-            for (var i = 0; i < 4; i++) {
-                var emplacement = i + 1;
-                document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
-            }
+            startTheGame(reponse, data);
             return 1;
         }
-        if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
-            console.log("Bravo !!  1+");
-            numberOfGoodAnswer++;
-        }
-        else {
-            console.log("Mauvaise Réponse !");
-        }
-        if (questionNumber === data.length) {
-            console.log(numberOfGoodAnswer);
-            console.log("Finie !");
-            document.getElementById("ecranFin").style.display = "initial";
-            document.getElementById("nombreDeReponse").innerHTML += numberOfGoodAnswer;
-            document.getElementById("content").style.display = "none";
-            return;
+        checkWin(reponse, data);
+        if (checkFin(reponse, data)) {
+            return 1;
         }
         document.getElementById("titreQuestion").innerHTML = data[questionNumber].nom;
         document.getElementById("Question").innerHTML = data[questionNumber].question;
@@ -47,4 +28,35 @@ function Quiz(reponse) {
         }
         questionNumber++;
     });
+}
+function startTheGame(reponse, data) {
+    console.log(questionNumber);
+    console.log("Start of the quiz");
+    document.getElementById("start").style.display = "none";
+    document.getElementById("content").style.display = "initial";
+    document.getElementById("titreQuestion").innerHTML = data[0].nom;
+    document.getElementById("Question").innerHTML = data[0].question;
+    for (var i = 0; i < 4; i++) {
+        var emplacement = i + 1;
+        document.getElementById(emplacement.toString()).innerHTML = data[0].reponses[0].Propositions[i];
+    }
+}
+function checkWin(reponse, data) {
+    if (reponse == data[questionNumber - 1].reponses[0].ReponsesVraie) {
+        console.log("Bravo !!  1+");
+        numberOfGoodAnswer++;
+    }
+    else {
+        console.log("Mauvaise Réponse !");
+    }
+}
+function checkFin(reponse, data) {
+    if (questionNumber === data.length) {
+        console.log(numberOfGoodAnswer);
+        console.log("Finie !");
+        document.getElementById("ecranFin").style.display = "initial";
+        document.getElementById("nombreDeReponse").innerHTML += numberOfGoodAnswer;
+        document.getElementById("content").style.display = "none";
+        return 1;
+    }
 }
