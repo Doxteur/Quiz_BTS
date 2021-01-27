4d1c98ca4cd812e2c5b82696d48b89b04e114204
doxteur <doxteurn@gmail.com>
2021-01-27 20:02:39 +0000

    Design and implementation of origin pierre

--- script.js
+++ script.js
@@ -43,6 +43,9 @@ function Quiz(reponse) {
 function startTheGame(reponse, data, nombreRandom, allQuestion) {
     console.log("Start of the quiz");
     document.getElementById("start").style.display = "none";
+    document.getElementById("wrapper").style.display = "none";
+    document.body.style.backgroundImage = "none";
+    document.getElementById("hamburger").style.display = "initial";
     document.getElementById("content").style.display = "initial";
     document.getElementById("Question").innerHTML = data[allQuestion[nombreRandom]].question;
     for (var i = 0; i < 4; i++) {
